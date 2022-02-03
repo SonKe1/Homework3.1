92185e9fcd2cd8c35296f0d7640fbb7a8c470447
SonKe1 <93735690+SonKe1@users.noreply.github.com>
2022-02-03 21:37:11 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,8 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int price=cost;
+        int price = cost;
         int bonus = 20;
         int mile = price / bonus;
-        if (price >= 0) {
-
-        }
         return mile;
 
     }
